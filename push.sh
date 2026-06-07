e5aa3015f2e96f277290edebaee3bf96c561b535
kritagya2055 <kritagyabhandari1999@gmail.com>
2026-06-07 13:09:39 +0545

    Sabina Bhandari funnel website

---
+++ push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /Users/shivalikabhandari22gmail.com/.gemini/antigravity/scratch/business-funnel
+git branch -M main
+git remote add origin https://github.com/kritagya2055/sabina-Bhandari.git
+git push -u origin main
